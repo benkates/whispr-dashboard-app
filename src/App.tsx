439b8cd6591dc68a6c8e80b24168420ffbbd0934
Ben Kates <benekates@gmail.com>
2025-04-11 14:28:35 -0400

    init page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            {/* parent styles lifted from shadcn/ui Dashboard block example https://ui.shadcn.com/blocks */}
+            {/* FUTURE ENHANCEMENT: only show filters once source api data has loaded */}
+            <div className="*:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4 grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card lg:px-6">
+              {/* AGE FILTER CARD */}
+              <Card className="@container/card">
+                <CardHeader className="relative">
+                  <CardTitle className="@[250px]/card:text-xl text-xl font-semibold tabular-nums">
+                    Age Group Filter
+                  </CardTitle>
+                </CardHeader>
+                <CardContent></CardContent>
+              </Card>
+              {/* GENDER FILTER CARD */}
+              <Card className="@container/card">
+                <CardHeader className="relative">
+                  <CardTitle className="@[250px]/card:text-xl text-xl font-semibold tabular-nums">
+                    Gender Filter
+                  </CardTitle>
+                </CardHeader>
+                <CardContent></CardContent>
+              </Card>
+            </div>
+            <div className="px-4 lg:px-6">
+              {" "}
+              <Card className="w-full p-4">
+                <h2>Question 2</h2>
+                <hr />
+                <h3>Question 3</h3>
+              </Card>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
